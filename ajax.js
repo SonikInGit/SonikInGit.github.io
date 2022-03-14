ecd14143ab2340c00e76e2461d854aef512ba9eb
SonikInGit <92980413+SonikInGit@users.noreply.github.com>
2022-03-14 15:26:11 +0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,21 +1,18 @@
-$( document ).ready(function() {
-    $('#button_email').click(
-		function(){
-		var email = $('#femail').val();
-        console.log(email);	
-        $.ajax({
-            url:     "../action_ajax_form.php", //url страницы (action_ajax_form.php)
-            type:     "POST", //метод отправки
-            data: email,
-            success: function(response) { 
-                result = jQuery.parseJSON(response); //Данные отправлены успешно
-                console.log("Email:"+ result);
-                document.getElementById("#success_subscribe").style.display = 'block';
-            },
-            error: function(response) { // Данные не отправлены
-                console.log('Ошибка. Данные не отправлены.');
-            }
-         });
-		});
-});
- 
+$(document).ready(function () {
+    $("email-form").submit(function (event) {
+      var formData = {
+        email: $("#femail").val(),
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "action_ajax_form.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      }).done(function (data) {
+        console.log(data);
+      });
+      event.preventDefault();
+    });
+  });
