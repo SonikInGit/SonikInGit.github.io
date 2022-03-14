3d90a71852d15a7c2a70c6ded132fed2decb8eb6
SonikInGit <92980413+SonikInGit@users.noreply.github.com>
2022-03-14 15:10:38 +0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,24 +1,21 @@
 $( document ).ready(function() {
-    $("#button_email").click(
+    $('#button_email').click(
 		function(){
-			sendAjaxForm('ajax_form', 'action_ajax_form.php');
-		}
-	);
+		var email = $('#femail').val();
+        console.log(email);	
+        $.ajax({
+            url:     "../action_ajax_form.php", //url страницы (action_ajax_form.php)
+            type:     "POST", //метод отправки
+            data: email,
+            success: function(response) { 
+                result = jQuery.parseJSON(response); //Данные отправлены успешно
+                console.log("Email:"+ result);
+                document.getElementById("#success_subscribe").style.display = 'block';
+            },
+            error: function(response) { // Данные не отправлены
+                console.log('Ошибка. Данные не отправлены.');
+            }
+         });
+		});
 });
  
-function sendAjaxForm(ajax_form, url) {
-    jQuery.ajax({
-        url:     "action_ajax_form.php", //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { 
-            result = jQuery.parseJSON(response); //Данные отправлены успешно
-            console.log("Email:"+ result.email);
-            document.getElementById("#success_subscribe").style.display = 'block';
-    	},
-    	error: function(response) { // Данные не отправлены
-            console.log('Ошибка. Данные не отправлены.');
-    	}
- 	});
-}
