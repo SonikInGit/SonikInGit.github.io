d16c7b22d8903b59b028565f07ec89f8d6910779
SonikInGit <92980413+SonikInGit@users.noreply.github.com>
2022-03-15 00:19:48 +0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $("email-form").submit(function (event) {
+    $("#email-form").submit(function (event) {
       $.ajax({
         type: "POST",
         url: "action_ajax_form.php",
         dataType: "html",
         success: function(response) { //Данные отправлены успешно
           result = $.parseJSON(response);
-          console.log("Email: " + result.email);
+          document.getElementById('#success_subscribe').style.display = 'block';
       },
       error: function(response) { // Данные не отправлены
         console.log("Ошибка. Данные не отправлены.");
