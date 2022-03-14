882c0df19e9d8897eb6b67a29e5a74a81509db3f
SonikInGit <92980413+SonikInGit@users.noreply.github.com>
2022-03-14 14:52:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if(window.innerWidth <= 768 || window.innerWidth < 1920){
           $("#hours").text("HH");
           $("#minutes").text("MM");
           $("#seconds").text("SS");
-          $(".label .back").attr("src","../test task/src/img/red_back2.svg");
+          $(".label .back").attr("src","../src/img/red_back2.svg");
         });
 }     
 else if(window.innerWidth >= 1920){
@@ -14,7 +14,7 @@ else if(window.innerWidth >= 1920){
         $("#hours").text("Hours");
         $("#minutes").text("Minutes");
         $("#seconds").text("Seconds");
-        $(".label .back").attr("src","../test task/src/img/red_back.svg");
+        $(".label .back").attr("src","../src/img/red_back.svg");
       });
 }
 }, true); 
